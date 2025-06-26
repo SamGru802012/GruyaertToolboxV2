2db00afc57d4500375c9188db01ddcbfb1503f8d
SamGru802012 <sam@gruyaert.be>
2025-06-26 09:35:23 +0200

    Delete utils directory

--- utils/optimizer.py
+++
@@ -1,7 +0,0 @@
-
-from utils.models import Oplossing
-def genereer_voorstellen(product, dikte):
-    oplossingen = []
-    for rot in range(6):
-        oplossingen.append(Oplossing(rotatie=rot, layout={}, score=0.85 - rot*0.05))
-    return oplossingen
