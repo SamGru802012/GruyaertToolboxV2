f29ada9cd071b36bee3fcc692ebf8ae41fcb0a69
SamGru802012 <sam@gruyaert.be>
2025-07-03 17:40:11 +0200

    Add files via upload

--- modules/utils.py
+++ modules/utils.py
@@ -1,7 +1,17 @@
 import streamlit as st
+import copy
 
 def initialize_session():
     if 'undo_stack' not in st.session_state:
         st.session_state.undo_stack = []
-    if 'selected_solution' not in st.session_state:
-        st.session_state.selected_solution = None
+    if 'solutions' not in st.session_state:
+        st.session_state.solutions = None
+
+def add_to_undo_stack(state_snapshot):
+    snapshot = copy.deepcopy(state_snapshot)
+    st.session_state.undo_stack.append(snapshot)
+
+def undo_last_action():
+    if st.session_state.undo_stack:
+        previous = st.session_state.undo_stack.pop()
+        st.session_state.solutions = previous
